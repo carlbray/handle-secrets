2e9a5e60eea7449b02eb0f2151023b523d2eec0f
Carl Bray <carl.bray@chorus.co.nz>
2025-08-03 17:06:01 +1200

    Disable prettier for key

--- test/cryptoHelper.test.ts
+++ test/cryptoHelper.test.ts
@@ -6,6 +6,7 @@ describe('cryptoHelper functions', () => {
   it('happy path', () => {
     // Arrange
     const plaintext = 'Sensitive data';
+    // eslint-disable-next-line prettier/prettier
     const key = '4ed15ad57ea8600a890ce1dfd50f123ecda476a205fc81d9decf2f7022b79cb6';
     const iv = 'a88d2d9b9c025c63dbb791c1';
 
